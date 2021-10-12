ed776c0c8da37a553cab0d7864d1a518606cb60f
Cipta Adinagara <ciptaadinagara@gmail.com>
2021-10-12 19:59:56 +0700

    Done

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from Detik.bmkg import ekstraksi_data, tampilkan_data
-from Detik.detik import tampilkan_data_detik, ekstraksi_data_detik
+from Package.bmkg import ekstraksi_data, tampilkan_data
+from Package.detik import tampilkan_data_detik, ekstraksi_data_detik
 
 if __name__ == '__main__':
     result = ekstraksi_data()
